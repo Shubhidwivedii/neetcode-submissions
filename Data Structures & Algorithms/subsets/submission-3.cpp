4d4bddf3039f65af03379d62b3e285ce83575d0e
Shubhidwivedii <dwivedishu03@gmail.com>
2026-09-03 00:30:10 +0530

    Add: subsets - submission-3

---
+++ Data Structures & Algorithms/subsets/submission-3.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void solve(int index,vector<int>&           nums,vector<vector<int>>&ans,vector<int>&ds){
+        if(index==nums.size()){
+            ans.push_back(ds);
+            return;
+        }
+        ds.push_back(nums[index]);
+        solve(index+1,nums,ans,ds);
+        ds.pop_back();
+        int next=index;
+        while(next<nums.size() && nums[index]==nums[next]) next++;
+        solve(index+1,nums,ans,ds);
+
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>ans;
+        vector<int>ds;
+        sort(nums.begin(),nums.end());
+        solve(0,nums,ans,ds);
+        return ans; 
+    }
+};
